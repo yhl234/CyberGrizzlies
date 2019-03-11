8c2edb43841a8369d593494a916fd50c7fecc9d4
yhl234 <43277189+yhl234@users.noreply.github.com>
2019-03-11 16:21:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,18 @@
 </head>
 <body>
 <?php
-	require '\connect.php';
+	$host = "mycgdb.mysql.database.azure.com";
+	$username = "cgadmin@mycgdb";
+	$password = "Password!";
+	$db_name = "mycgdb";
+
+	//Establishes the connection
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+	if (mysqli_connect_errno($conn)) {
+	die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+
 	echo '<p>connect is good</p>';
 	mysqli_close($conn); 
 ?>
