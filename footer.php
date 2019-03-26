80fe83d1fbe7ccca0e7eb1303c0b819d3ade3bdf
DanZedd <200254829@student.georgianc.on.ca>
2019-03-25 22:42:31 -0400

    test

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 <!-- <script src="js/main.js"></script> -->
 </main>
 <footer>
-<p>&#xa9 group 3 2019 - all rights reserved<img src="images/discord.svg" alt="discord icon" class="footer-icon" ></p> 
+<p>&#xa9 group 3 2019 - all rights reserved</p> 
 <hr>
+<ul>
+<li><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
+<li><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
+<li><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
 </footer>
 </main>
 </body>
