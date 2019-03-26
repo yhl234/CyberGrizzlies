d1ee02c48797bc88cd685faa93a09f4e6379ff7d
DanZedd <200254829@student.georgianc.on.ca>
2019-03-25 22:26:30 -0400

    test

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!-- <script src="js/main.js"></script> -->
 </main>
 <footer>
-<p>&#xa9 group 3 2019 - all rights reserved</p>
+<p>&#xa9 group 3 2019 - all rights reserved</p> <i class="fab fa-discord"></i>
 <hr>
 </footer>
 </main>
