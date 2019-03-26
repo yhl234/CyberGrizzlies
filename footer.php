ea6f5c6357f85bc13d4777a5508d1ba43d78b16a
DanZedd <200254829@student.georgianc.on.ca>
2019-03-25 22:46:16 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 <p>&#xa9 group 3 2019 - all rights reserved</p> 
 <hr>
 <ul>
-<li><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
-<li><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
-<li><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
+<li><a href="wowhead.com"><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
+<li><a href="wowhead.com"><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
+<li><a href="wowhead.com"><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
 </footer>
 </main>
 </body>
