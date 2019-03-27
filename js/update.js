287389771ecd16e7dd20ad91575b4e8381dac082
yhl234 <200392342@student.georgianc.on.ca>
2019-03-27 16:26:40 -0400

    hide and show

--- js/update.js
+++ js/update.js
@@ -1,13 +1,28 @@
 var selected = document.querySelector('select');
+var view = document.querySelector('main')
+var tables = ['Player', 'Status', 'Attendee', 'Event', 'EventType', 'Location', 'Game', 'Genre', 'GamePlatform', 'Stream', 'StreamPlatform', 'User'];
 
 function showTable(){
 	//get value from input
 	var tableID = selected.value.toString();
 	//use that value to query
-	var tableName = document.getElementById(tableID);
+	for ( var i = 0; i < 12; i++){
+		var hided = document.getElementById(tables[i]);
+		var display = document.getElementById(tableID);
+		if (tableID == tables[i]){
+			display.style.display = 'unset';
+		} else{
+			hided.style.display = 'none';
+		}
+	}
 	//show select table
-	tableName.style.display = 'unset';
 }
-selected.addEventListener('input', function () {
+
+function cleaner(){
+	view.innerHTML = '';
+}
+
+selected.addEventListener('change', function () {
 	showTable();
 });
+
