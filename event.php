0a73ae08dc7eb1ab2b5818c67a23888db277cb1c
DanZedd <200254829@student.georgianc.on.ca>
2019-03-26 22:21:25 -0400

    Update event.php

--- event.php
+++ event.php
@@ -33,7 +33,8 @@
 		</div>';
 
 		echo '<br /><label for="eventDate">Event date:</label>';
-		echo '<input type="date" id="eventDate" name="eventDate" /><input type="time" name="eventTime" id="eventTime" />';
+    echo '<input type="date" placeholder="Event Type" id="eventDate" name="eventDate" />
+          <input type="time" name="eventTime" id="eventTime" />';
 		
 		echo '<br /><label for="locationId">Location:</label>';
 
