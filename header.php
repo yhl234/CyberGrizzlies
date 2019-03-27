f9dcfc1c5d8945c397fb1640a77cd56bc6f01641
DanZedd <200254829@student.georgianc.on.ca>
2019-03-26 23:01:05 -0400

    test

--- header.php
+++ header.php
@@ -13,8 +13,7 @@
 <header>
   <nav>
     <ul>
-      <li><a class="border-link" href="http://localhost/CyberGrizzlies/CyberGrizzlies/landing.html">#home</a></li>
-      <li><a class="border-link" href="#">#create</a></li>
+      <li><a class="border-link" href="http://localhost/CyberGrizzlies/CyberGrizzlies/landing.html">#create</a></li>
       <li><a class="border-link" href="#">#edit</a></li>
       <li><a class="border-link" href="#">#reports</a></li>
     </ul>
