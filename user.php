a5906c3ec364214a319da14594af16ac322547e5
Ryan-Gee <44777268+Ryan-Gee1@users.noreply.github.com>
2019-03-30 00:49:27 -0400

    First pass commit
    First pass done on the user form! Allows the creation of endless unique streaming subforms, still no subform under those for new streaming platforms (that's on it's way!)

--- user.php
+++ user.php
@@ -15,89 +15,86 @@
 	<input type="text" id="middleName" name="middleName" />
 	<label for="lastName">Last Name:</label>
 	<input type="text" id="lastName" name="lastName" />
-	<label for="startDAte">Start Date:*</label>
+	<label for="startDate">Start Date:*</label>
     <input type="date" id="startDate" name="startDate" required/>
 	<label for="active">Currently Active:*</label>
 	<input type="checkbox" name="active" id="active" required checked/>
 	<label for="payStatus">Pay Status:*</label>
 	<input type="checkbox" name="payStatus" id="payStatus" required/>
-	<label for="email">Email:*</label>
-	<input type="text" id="email" placeholder="Email Address" name="email" />
+	<label for="email">Email:</label>
+	<input type="email" id="email" placeholder="you@email.com" name="email" />
 	<label for="phone">Phone Number:</label>
-	<input id="phone" name="phone" type="tel" required pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="xxx-xxx-xxxx" />
+	<input id="phone" name="phone" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="xxx-xxx-xxxx" />
+	<label for="chatStatus">Active in Chat:*</label>
+	<input type="checkbox" name="chatStatus" id="chatStatus" required/>
+	<label for="discord">Discord Tag:</label>
+	<input id="discord" name="discord" type="text" pattern="[a-z0-9._%+-]+#[0-9]{4}$" placeholder="username#0000" />
 
+	<button id="addStream" >+Add Streaming Platform</button>
+	<input type="submit" value="Submit" />
+	<input type="hidden" id="streamCount" value=0 />
+</form>
+<!-- So I was in fact a terrible person and put inline JS into the HTML, but it was the only way I could make it work -->
+<?php
+	$platformSql = "SELECT StreamPlatformID, StreamPlatformName FROM StreamPlatform";
+	$platforms = mysqli_query($conn, $platformSql);
 
+	$platformNames = array();
+	$platformIds = array();
+	while ($row = mysqli_fetch_array($platforms)) {
+		$platformIds[] = $row['StreamPlatformID'];
+		$platformNames[] = $row['StreamPlatformName'];
+	}
 
+	$platformNames = json_encode($platformNames);
+	$platformIds = json_encode($platformIds);
+?>
+<script type="text/javascript">
 
+	var platformIds = <?php echo json_encode($platformIds) ?>;
+	var platformNames = <?php echo json_encode($platformNames) ?>;
 
+	var platformIds = JSON.parse(platformIds);
+	var platformNames = JSON.parse(platformNames);
 
+	var addStream = document.getElementById("addStream");
+	var streamCount = 0;
+	var streamCounter = document.getElementById("streamCount");
 
+	addStream.onclick = addStreamForm;
 
+	function addStreamForm () {
+		streamCount += 1;
+		streamCounter.value = streamCount;
+		var subform = document.createElement("div");
+		subform.id = "streamSubForm";
 
-
-
-	<!-- Start php -->
-	<?php
-		$sql = "SELECT eventtypeid, eventtypename FROM eventtype ORDER BY eventtypename";
-		$types = mysqli_query($conn, $sql);
-
-		echo '<select id="eventType" name="eventType" required>';
-
-		while ($type = mysqli_fetch_array($types)) {
-			$typeId = $type['eventtypeid'];
-			$tname = $type['eventtypename'];
-			echo '<option value="' . $typeId . '">' . $tname . '</option>';
+		var streamPlatform = document.createElement("select");
+		streamPlatform.name = "streamPlatform" + streamCount;
+		streamPlatform.required = true;
+		for (var i = 0; i < platformIds.length; i++)
+		{
+			var option = document.createElement("option");
+			option.value = platformIds[i];
+			option.textContent = platformNames[i];
+			streamPlatform.appendChild(option);
+			
 		}
-		echo '<option value="-1">Other</option>';
-		// End of type select box
-		echo '</select>';
-		
-		echo '
-		<div id="typeSubForm" class="hidden">
-			<label for="newType">New Event</label>
-			<input type="text" name="newType" id="newType" />
-		</div>';
+		subform.appendChild(streamPlatform);
 
-		echo '<br /><label for="eventDate">Event date:</label>';
-    echo '<input type="date" id="eventDate" name="eventDate" />
-          <input type="time" name="eventTime" id="eventTime" />';
-		
-		echo '<br /><label for="locationId">Location:</label>';
+		var urlLabel = document.createElement("label");
+		urlLabel.setAttribute("for", "streamUrl" + streamCount);
+		urlLabel.textContent = "Stream URL:*";
+		subform.appendChild(urlLabel);
 
+		var url = document.createElement("input");
+		url.type = "text";
+		url.name = "streamUrl" + streamCount;
+		url.id = url.name;
+		subform.appendChild(url);
 
-		
-		$sql = "SELECT locationid, locationname FROM location ORDER BY locationid";
-		$locs = mysqli_query($conn, $sql);
+		document.querySelector("form").insertBefore(subform, addStream);
+	}
 
-		echo '<select id="locationId" name="locationId" required>';
-
-		while ($loc = mysqli_fetch_array($locs)) {
-			$locId = $loc['locationid'];
-			$lname = $loc['locationname'];
-			echo '<option value="' . $locId . '">' . $lname . '</option>';
-		}
-		echo '<option value="-1">Other</option>';	
-		// End of type select box
-		echo '</select>';
-
-		echo '
-		<div id="locSubForm" class="hidden">
-			<label for="newLocName">New location name:</label>
-			<input type="text" name="newLocName" id="newLocName" />
-			<br /><label for="newLocAddr">New location address:</label>
-			<input type="text" name="newLocAddr" id="newLocAddr" />
-		</div>';
-
-		echo '<br /><label for="eventDesc">Event Description</label>';
-		echo '<textarea id="eventDesc" name="eventDesc"></textarea>';
-
-		// Form submit button
-		echo '<br /><input type="submit" value="Submit" />';
-
-	//End form
-	echo '</form>';
-	// Include script to handle subform hiding
-  echo '<script src="js/event.js"></script>';
-	//End file
-	require 'footer.php';
-	?>
+</script>
+<?php require 'footer.php'; ?>
