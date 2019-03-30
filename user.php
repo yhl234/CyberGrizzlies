496b958b1e8a5d483d9a87cd2f0dac34a42a7a18
Ryan-Gee <44777268+Ryan-Gee1@users.noreply.github.com>
2019-03-30 11:10:36 -0400

    Finished user.php!
    First pass done for user.php! Everything works (no submission yet) with multiple parallel subforms to add streaming links and secondary subforms on those to allow addition of new streaming platforms.

--- user.php
+++ user.php
@@ -30,7 +30,7 @@
 	<label for="discord">Discord Tag:</label>
 	<input id="discord" name="discord" type="text" pattern="[a-z0-9._%+-]+#[0-9]{4}$" placeholder="username#0000" />
 
-	<button id="addStream" >+Add Streaming Platform</button>
+	<button id="addStream" type="button">+Add Streaming Platform</button>
 	<input type="submit" value="Submit" />
 	<input type="hidden" id="streamCount" value=0 />
 </form>
@@ -67,7 +67,12 @@
 		streamCount += 1;
 		streamCounter.value = streamCount;
 		var subform = document.createElement("div");
-		subform.id = "streamSubForm";
+		subform.id = "stream" + streamCount + "SubForm";
+
+		var selectLabel = document.createElement("label");
+		selectLabel.setAttribute("for", "streamPlatform" + streamCount);
+		selectLabel.textContent = "Stream Platform:*";
+		subform.appendChild(selectLabel);
 
 		var streamPlatform = document.createElement("select");
 		streamPlatform.name = "streamPlatform" + streamCount;
@@ -78,9 +83,36 @@
 			option.value = platformIds[i];
 			option.textContent = platformNames[i];
 			streamPlatform.appendChild(option);
-			
 		}
+
+		var option = document.createElement("option");
+		option.value = -1;
+		option.textContent = "Other";
+		streamPlatform.appendChild(option);
+
 		subform.appendChild(streamPlatform);
+
+		// Create second level sub form here
+		var platformSubform = document.createElement("div");
+		platformSubform.id = "platform" + streamCount + "SubForm";
+		platformSubform.className = "hidden";
+		
+		var newPlatformLabel = document.createElement("label");
+		newPlatformLabel.for = "newPlatform" + streamCount;
+		newPlatformLabel.textContent = "New Streaming Platform Name:";
+		platformSubform.appendChild(newPlatformLabel);
+
+		var newPlatform = document.createElement("input");
+		newPlatform.type = "text";
+		newPlatform.name = "newPlatform" + streamCount;
+		newPlatform.id = newPlatform.name;
+		platformSubform.appendChild(newPlatform);
+
+		streamPlatform.onchange = function () {
+			UpdateSelect(streamPlatform.value, platformSubform);
+		}
+		subform.appendChild(platformSubform);
+		// End of second level sub form
 
 		var urlLabel = document.createElement("label");
 		urlLabel.setAttribute("for", "streamUrl" + streamCount);
@@ -94,6 +126,17 @@
 		subform.appendChild(url);
 
 		document.querySelector("form").insertBefore(subform, addStream);
+	}
+
+	function UpdateSelect (val, form) {
+		console.log("got to update select function");
+		if (val == -1) {
+			form.className = "";
+			console.log("Subform revealed");
+		}
+		else {
+			form.className = "hidden";
+		}
 	}
 
 </script>
