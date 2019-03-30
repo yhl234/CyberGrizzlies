4588df98d1f3d3a735c525a48080f0af5885aad3
Ryan-Gee <44777268+Ryan-Gee1@users.noreply.github.com>
2019-03-30 18:44:02 -0400

    Top level pass on player.php
    First top level pass on player.php, subforms still need JS to work properly

--- player.php
+++ player.php
@@ -6,6 +6,8 @@
 <h1>create player</h1>
 <form action="add_player.php" method="post">
 
+	<label for="userId">User:</label>
+
 	<?php
 		$sql = "SELECT userId, username, firstName, lastName FROM user ORDER BY userId";
 		$users = mysqli_query($conn, $sql);
@@ -47,6 +49,7 @@
 			echo '<input type="checkbox" name="multiplayer" id="multiplayer" />';
 			echo '<label for="genreId">Genre:</label>';
 
+
 			$sql = "SELECT genreid, genrename FROM genre ORDER BY genrename";
 			$genres = mysqli_query($conn, $sql);
 
@@ -59,15 +62,74 @@
 			}
 			echo '<option value="-1">Other</option>';
 			echo '</select>';
-
+			// Second level subform
 			echo '<div id="genreSubForm" class="hidden">';
 				echo '<label for="newGenreName">New Genre Name:</label>';
 				echo '<input type="text" name="newGenreName" id="newGenreName" required />';
+			// End of second level subform
 			echo '</div>';
+		// End of first level subform
 		echo '</div>';
 
 
-		// Form submit button
+		// Status selection
+		echo '<label for="statusId">Player Status:</label>';
+		$sql = "SELECT statusId, statusname FROM status ORDER BY statusname";
+		$statuses = mysqli_query($conn, $sql);
+
+		echo '<select id="statusId" name="statusId" required>';
+
+		while ($status = mysqli_fetch_array($statuses)) {
+			$statusId = $status['statusId'];
+			$sname = $status['statusname'];
+
+			echo '<option value="' . $statusId . '">' . $sname . '</option>';
+		}
+		echo '<option value=-1>Other</option>';
+		echo '</select>';
+
+		// Start of status subform
+		echo '<div id="statusSubForm" class="hidden">';
+			echo '<br /><label for="newStatus">New Status:</label>';
+			echo '<input type="text" name="newStatus" id="newStatus"/>';
+		echo '</div>';
+
+
+		echo '<br /><label for="locationId">Usual Play Location:</label>';
+		$sql = "SELECT locationid, locationname FROM location ORDER BY locationid";
+		$locs = mysqli_query($conn, $sql);
+
+		echo '<select id="locationId" name="locationId" required>';
+
+		while ($loc = mysqli_fetch_array($locs)) {
+			$locId = $loc['locationid'];
+			$lname = $loc['locationname'];
+			echo '<option value="' . $locId . '">' . $lname . '</option>';
+		}
+		echo '<option value="-1">Other</option>';	
+		echo '</select>';
+
+
+		echo '<br /><label for="platformId">Game Platform:</label>';
+		$sql = "SELECT gameplatformid, gameplatformname FROM gameplatform ORDER BY gameplatformname";
+		$plats = mysqli_query($conn, $sql);
+
+		echo '<select id="platformId" name="platformId" required>';
+
+		while ($plat = mysqli_fetch_array($plats)) {
+			$platId = $plat['gameplatformid'];
+			$platName = $plat['gameplatformname'];
+			echo '<option value="' . $platId . '">' . $platName . '</option>';
+		}
+		echo '<option value="-1">Other</option>';	
+		echo '</select>';
+		echo '<div id="platformSubForm" class="hidden">';
+			echo '<br /><label for="newPlatformName">New Platform Name:</label>';
+			echo '<input type="text" name="newPlatformName" id="newPlatformName"/>';
+		echo '</div>';
+
+
+		// Form submit 
 		echo '<br /><input type="submit" value="Submit" />';
 
 	//End form
