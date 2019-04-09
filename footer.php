31634233c3243de2785b8e90312ed3f4e993844e
DanZedd <200254829@student.georgianc.on.ca>
2019-04-09 12:04:45 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,6 @@
 		<footer>
       <hr />
 			<p>&#xa9 group 3 2019 - all rights reserved</p> 
-			<!-- <hr> -->
 			<ul>
 				<li><a href="#"><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
 				<li><a href="#"><img src="images/twitch.svg" alt="twitch icon" class="footer-icon" ></li>
