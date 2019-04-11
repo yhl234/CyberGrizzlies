41b064703dfa20e41990c0370f285a8fc5fd6bb6
DanZedd <200254829@student.georgianc.on.ca>
2019-04-11 11:46:09 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,11 +2,11 @@
 		</main>
 		<footer>
       <hr />
-			<p>&#xa9 group 3 2019 - all rights reserved</p> 
+			<p>&#xa9 Cyber Grizzlies 2019 - all rights reserved</p> 
 			<ul>
-				<li><a href="#"><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
-				<li><a href="#"><img src="images/twitch.svg" alt="twitch icon" class="footer-icon" ></li>
-				<li><a href="#"><img src="images/youtube.svg" alt="youtube icon" class="footer-icon" ></li>
+				<li><a href="https://discordapp.com/"><img src="images/discord.svg" alt="discord icon" class="footer-icon" ></li>
+				<li><a href="https://www.twitch.tv/"><img src="images/twitch.svg" alt="twitch icon" class="footer-icon" ></li>
+				<li><a href="https://www.youtube.com/"><img src="images/youtube.svg" alt="youtube icon" class="footer-icon" ></li>
 			</ul>
 		</footer>
 	<!-- </main> -->
