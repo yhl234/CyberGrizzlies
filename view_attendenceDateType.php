d6b48e446382d0ae55b04d496159e2134cf5d6fe
yhl234 <200392342@student.georgianc.on.ca>
2019-04-11 17:11:42 -0400

    js

--- view_attendenceDateType.php
+++ view_attendenceDateType.php
@@ -7,19 +7,6 @@
 
 <?php 
 
-	$eventId = $_GET['EventId'];
-	if (!$eventId) {
-		$eventId = 1;
-	}
-	// $startDate = $_GET['startDate'];
-	// $endDate = $_GET['endDate'];
-	// $sql = "";
-	$query = true;
-
-	if (empty($eventId)) {
-		$query = false;
-	}
-
   echo '
   <div>
   <label for="EventId">Events:</label>';
@@ -32,22 +19,14 @@
 		$eventId = $event['EventId'];
         $eventName = $event['EventName'];
         $eventDateTime = $event['EventDateTime'];
-		if ($id == $eventId) {
-			echo '<option value="' . $eventId . '" selected>' . $eventName . ' (' . $eventDateTime . ')</option>';
-		}
-		else {
-			echo '<option value="' . $eventId . '" selected>' . $eventName . ' (' . $eventDateTime . ')</option>';
-		}
+			echo '<option value="view_attendenceDateType.php?id=' . $eventId . '">' . $eventName . ' (' . $eventDateTime . ')</option>';
 	}
   echo '
   </div>
   </select>';
+	$id = $_GET['id'];
 
-
-	$sql = "";
-
-		if ($query) {
-			$sql = "SELECT username, firstname, lastname, isremote FROM Attendee INNER JOIN user WHERE `EventID` = $eventId";
+	$sql = "SELECT username, firstname, lastname, isremote FROM Attendee INNER JOIN user WHERE `EventID` = $id";
             
             
             echo '
@@ -79,14 +58,10 @@
             echo '
             </div>
             </table>';
-		}
-		else {
-			echo 'No events could be found!';
-		}
 
 require 'footer.php';
 ?>
-
+<script src="js/view_attendenceDateType.js" defer></script>
 
 <!--
     
